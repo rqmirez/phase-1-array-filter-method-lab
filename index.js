79323c71b22f5fa43967dfbd6c26ec0f300c5f77
Judith Ramirez <rqmirez@gmail.com>
2022-06-06 13:30:45 -0400

    commit tree e0da22536e010feb652381e9e715711213a9b855

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(drivers, string){
+    const answer = drivers.filter(driversName => driversName.toLowerCase () === string.toLowerCase())
+    return answer
+}
+
+function fuzzyMatch(drivers, string){
+    const answer = drivers.filter(driversName => driversName.startsWith(string))
+    return answer
+}
+
+function matchName(drivers, name){
+    return drivers.filter(function(hometown){
+      return hometown.name === name
+    })
+  }
